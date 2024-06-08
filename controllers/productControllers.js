03504b7a26a4f07e99390826e2599cf5a86bef9d
rakiburrahman307 <rakiburrahman307@gmail.com>
2024-06-09 02:15:41 +0600

    product filter sorting  functionality added done

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -40,7 +40,9 @@ const relatedProduct = async (req, res) => {
 };
 const filteredProducts = async (req, res) => {
   try {
-    const { categories, color, size, brand, minPrice, maxPrice, rating } = req.body;
+    const { categories, color, size, brand, minPrice, maxPrice, rating, sort } =
+      req.body;
+    const finalRating = parseFloat(rating?.split(",")[0]);
     // Input Validation
     if (minPrice && (isNaN(minPrice) || minPrice < 0)) {
       return res.status(400).json({ error: "Invalid minPrice format" });
@@ -48,7 +50,10 @@ const filteredProducts = async (req, res) => {
     if (maxPrice && (isNaN(maxPrice) || maxPrice < 0)) {
       return res.status(400).json({ error: "Invalid maxPrice format" });
     }
-    if (rating && (isNaN(rating) || rating < 0 || rating > 5)) {
+    if (
+      finalRating &&
+      (isNaN(finalRating) || finalRating < 0 || finalRating > 5)
+    ) {
       return res.status(400).json({ error: "Invalid rating format" });
     }
     // Input Validation close
@@ -67,25 +72,50 @@ const filteredProducts = async (req, res) => {
       query.brand = brand;
     }
 
-    if (minPrice && maxPrice) {
+    if (minPrice > 2 && maxPrice > 2) {
       query.price = { $gte: parseFloat(minPrice), $lte: parseFloat(maxPrice) };
-    } else if (minPrice) {
+    } else if (minPrice > 2) {
       query.price = { $gte: parseFloat(minPrice) };
-    } else if (maxPrice) {
+    } else if (maxPrice > 2) {
       query.price = { $lte: parseFloat(maxPrice) };
     }
 
-    if (rating) {
-      query.rating = { $gte: parseFloat(rating) };
+    if (finalRating) {
+      query.rating = { $gte: finalRating };
     }
-    const products = await Product.find(query).lean();
+
+    // Sorting logic
+    let sortCriteria = {};
+    switch (sort) {
+      case "price_high_to_low":
+        sortCriteria.price = -1;
+        break;
+      case "price_low_to_high":
+        sortCriteria.price = 1;
+        break;
+      case "a_to_z":
+        sortCriteria.title = 1;
+        break;
+      case "z_to_a":
+        sortCriteria.title = -1;
+        break;
+      case "best_rating":
+        sortCriteria.rating = -1;
+        break;
+      case "newest":
+        sortCriteria.createdAt = -1;
+        break;
+      default:
+        sortCriteria = {};
+    }
+
+    const products = await Product.find(query).sort(sortCriteria).lean();
     res.status(200).json(products);
   } catch (error) {
     console.error("Error fetching products:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 };
-
 
 module.exports = {
   getProducts,
